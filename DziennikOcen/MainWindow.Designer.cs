f99e9a72bcc25bb6c2764e222efd334cf5311f60
kstud <kstud@DESKTOP-I0CRGQB>
2016-06-06 22:29:11 +0200

    Prawdopodobnie udało mi się zrobić wyświetlanie studentów.

--- DziennikOcen/MainWindow.Designer.cs
+++ DziennikOcen/MainWindow.Designer.cs
@@ -34,72 +34,59 @@
             this.buttonProjekty = new System.Windows.Forms.Button();
             this.buttonOceny = new System.Windows.Forms.Button();
             this.buttonPrzedmioty = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.studenciBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.studentidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.imieDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.nazwiskoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.nrindexuDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.kierunekDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.grupaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.hasloDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.ocenyprojektBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.textBoxOcena = new System.Windows.Forms.TextBox();
-            this.buttonZmienOcene = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.studenciBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ocenyprojektBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.25F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.75F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 531F));
             this.tableLayoutPanel1.Controls.Add(this.buttonStudenci, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.buttonProjekty, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.buttonOceny, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.buttonPrzedmioty, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.button5, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.button6, 1, 4);
-            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.textBoxOcena, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.buttonZmienOcene, 2, 4);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 5;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 16F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(806, 473);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(604, 384);
             this.tableLayoutPanel1.TabIndex = 0;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // buttonStudenci
             // 
             this.buttonStudenci.AutoSize = true;
-            this.buttonStudenci.Location = new System.Drawing.Point(3, 3);
+            this.buttonStudenci.Location = new System.Drawing.Point(2, 2);
+            this.buttonStudenci.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonStudenci.Name = "buttonStudenci";
-            this.buttonStudenci.Size = new System.Drawing.Size(75, 27);
+            this.buttonStudenci.Size = new System.Drawing.Size(59, 23);
             this.buttonStudenci.TabIndex = 0;
             this.buttonStudenci.Text = "Studenci";
             this.buttonStudenci.UseVisualStyleBackColor = true;
+            this.buttonStudenci.Click += new System.EventHandler(this.buttonStudenci_Click);
             // 
             // buttonProjekty
             // 
             this.buttonProjekty.AutoSize = true;
-            this.buttonProjekty.Location = new System.Drawing.Point(102, 3);
+            this.buttonProjekty.Location = new System.Drawing.Point(220, 2);
+            this.buttonProjekty.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonProjekty.Name = "buttonProjekty";
-            this.buttonProjekty.Size = new System.Drawing.Size(75, 27);
+            this.buttonProjekty.Size = new System.Drawing.Size(56, 23);
             this.buttonProjekty.TabIndex = 1;
             this.buttonProjekty.Text = "Projekty";
             this.buttonProjekty.UseVisualStyleBackColor = true;
@@ -107,9 +94,10 @@
             // buttonOceny
             // 
             this.buttonOceny.AutoSize = true;
-            this.buttonOceny.Location = new System.Drawing.Point(3, 159);
+            this.buttonOceny.Location = new System.Drawing.Point(2, 29);
+            this.buttonOceny.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonOceny.Name = "buttonOceny";
-            this.buttonOceny.Size = new System.Drawing.Size(75, 27);
+            this.buttonOceny.Size = new System.Drawing.Size(56, 23);
             this.buttonOceny.TabIndex = 2;
             this.buttonOceny.Text = "Oceny";
             this.buttonOceny.UseVisualStyleBackColor = true;
@@ -117,138 +105,48 @@
             // buttonPrzedmioty
             // 
             this.buttonPrzedmioty.AutoSize = true;
-            this.buttonPrzedmioty.Location = new System.Drawing.Point(102, 159);
+            this.buttonPrzedmioty.Location = new System.Drawing.Point(220, 29);
+            this.buttonPrzedmioty.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonPrzedmioty.Name = "buttonPrzedmioty";
-            this.buttonPrzedmioty.Size = new System.Drawing.Size(88, 27);
+            this.buttonPrzedmioty.Size = new System.Drawing.Size(68, 23);
             this.buttonPrzedmioty.TabIndex = 3;
             this.buttonPrzedmioty.Text = "Przedmioty";
             this.buttonPrzedmioty.UseVisualStyleBackColor = true;
             // 
-            // button5
-            // 
-            this.button5.AutoSize = true;
-            this.button5.Location = new System.Drawing.Point(3, 192);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 27);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // button6
             // 
             this.button6.AutoSize = true;
-            this.button6.Location = new System.Drawing.Point(102, 192);
+            this.button6.Location = new System.Drawing.Point(220, 56);
+            this.button6.Margin = new System.Windows.Forms.Padding(2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 27);
+            this.button6.Size = new System.Drawing.Size(56, 23);
             this.button6.TabIndex = 5;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
             // 
-            // dataGridView1
+            // button5
             // 
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.studentidDataGridViewTextBoxColumn,
-            this.imieDataGridViewTextBoxColumn,
-            this.nazwiskoDataGridViewTextBoxColumn,
-            this.nrindexuDataGridViewTextBoxColumn,
-            this.kierunekDataGridViewTextBoxColumn,
-            this.grupaDataGridViewTextBoxColumn,
-            this.emailDataGridViewTextBoxColumn,
-            this.hasloDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.studenciBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(277, 3);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(468, 150);
-            this.dataGridView1.TabIndex = 6;
-            // 
-            // studenciBindingSource
-            // 
-            this.studenciBindingSource.DataSource = typeof(DziennikOcen.studenci);
-            // 
-            // studentidDataGridViewTextBoxColumn
-            // 
-            this.studentidDataGridViewTextBoxColumn.DataPropertyName = "student_id";
-            this.studentidDataGridViewTextBoxColumn.HeaderText = "student_id";
-            this.studentidDataGridViewTextBoxColumn.Name = "studentidDataGridViewTextBoxColumn";
-            // 
-            // imieDataGridViewTextBoxColumn
-            // 
-            this.imieDataGridViewTextBoxColumn.DataPropertyName = "imie";
-            this.imieDataGridViewTextBoxColumn.HeaderText = "imie";
-            this.imieDataGridViewTextBoxColumn.Name = "imieDataGridViewTextBoxColumn";
-            // 
-            // nazwiskoDataGridViewTextBoxColumn
-            // 
-            this.nazwiskoDataGridViewTextBoxColumn.DataPropertyName = "nazwisko";
-            this.nazwiskoDataGridViewTextBoxColumn.HeaderText = "nazwisko";
-            this.nazwiskoDataGridViewTextBoxColumn.Name = "nazwiskoDataGridViewTextBoxColumn";
-            // 
-            // nrindexuDataGridViewTextBoxColumn
-            // 
-            this.nrindexuDataGridViewTextBoxColumn.DataPropertyName = "nr_indexu";
-            this.nrindexuDataGridViewTextBoxColumn.HeaderText = "nr_indexu";
-            this.nrindexuDataGridViewTextBoxColumn.Name = "nrindexuDataGridViewTextBoxColumn";
-            // 
-            // kierunekDataGridViewTextBoxColumn
-            // 
-            this.kierunekDataGridViewTextBoxColumn.DataPropertyName = "kierunek";
-            this.kierunekDataGridViewTextBoxColumn.HeaderText = "kierunek";
-            this.kierunekDataGridViewTextBoxColumn.Name = "kierunekDataGridViewTextBoxColumn";
-            // 
-            // grupaDataGridViewTextBoxColumn
-            // 
-            this.grupaDataGridViewTextBoxColumn.DataPropertyName = "grupa";
-            this.grupaDataGridViewTextBoxColumn.HeaderText = "grupa";
-            this.grupaDataGridViewTextBoxColumn.Name = "grupaDataGridViewTextBoxColumn";
-            // 
-            // emailDataGridViewTextBoxColumn
-            // 
-            this.emailDataGridViewTextBoxColumn.DataPropertyName = "email";
-            this.emailDataGridViewTextBoxColumn.HeaderText = "email";
-            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
-            // 
-            // hasloDataGridViewTextBoxColumn
-            // 
-            this.hasloDataGridViewTextBoxColumn.DataPropertyName = "haslo";
-            this.hasloDataGridViewTextBoxColumn.HeaderText = "haslo";
-            this.hasloDataGridViewTextBoxColumn.Name = "hasloDataGridViewTextBoxColumn";
-            // 
-            // ocenyprojektBindingSource
-            // 
-            this.ocenyprojektBindingSource.DataSource = typeof(DziennikOcen.oceny_projekt);
-            // 
-            // textBoxOcena
-            // 
-            this.textBoxOcena.Location = new System.Drawing.Point(277, 159);
-            this.textBoxOcena.Name = "textBoxOcena";
-            this.textBoxOcena.Size = new System.Drawing.Size(256, 22);
-            this.textBoxOcena.TabIndex = 8;
-            // 
-            // buttonZmienOcene
-            // 
-            this.buttonZmienOcene.AutoSize = true;
-            this.buttonZmienOcene.Location = new System.Drawing.Point(277, 192);
-            this.buttonZmienOcene.Name = "buttonZmienOcene";
-            this.buttonZmienOcene.Size = new System.Drawing.Size(100, 27);
-            this.buttonZmienOcene.TabIndex = 9;
-            this.buttonZmienOcene.Text = "Zmień ocene";
-            this.buttonZmienOcene.UseVisualStyleBackColor = true;
-            this.buttonZmienOcene.Click += new System.EventHandler(this.buttonZmienOcene_Click);
+            this.button5.AutoSize = true;
+            this.button5.Location = new System.Drawing.Point(2, 56);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(56, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(806, 473);
+            this.ClientSize = new System.Drawing.Size(604, 384);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainWindow";
             this.Text = "MainWindow";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.studenciBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ocenyprojektBindingSource)).EndInit();
             this.ResumeLayout(false);
@@ -262,9 +160,6 @@
         private System.Windows.Forms.Button buttonProjekty;
         private System.Windows.Forms.Button buttonOceny;
         private System.Windows.Forms.Button buttonPrzedmioty;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.BindingSource ocenyprojektBindingSource;
         private System.Windows.Forms.BindingSource studenciBindingSource;
         private System.Windows.Forms.DataGridViewTextBoxColumn studentidDataGridViewTextBoxColumn;
@@ -275,7 +170,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn grupaDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn hasloDataGridViewTextBoxColumn;
-        private System.Windows.Forms.TextBox textBoxOcena;
-        private System.Windows.Forms.Button buttonZmienOcene;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- DziennikOcen/MainWindow.cs
+++ DziennikOcen/MainWindow.cs
@@ -13,39 +13,49 @@ namespace DziennikOcen
     public partial class MainWindow : Form
     {
         dziennik_ocenEntities doe;
+        DataGridView dataGridViewStudents;
 
         public MainWindow()
         {
             InitializeComponent();
             doe = new dziennik_ocenEntities();
-            //ocenyprojektBindingSource.DataSource = doe.oceny_projekt;
 
-            ocenyprojektBindingSource.DataSource = (from temp in doe.oceny_projekt
-                                 select new { temp.student_id, temp.projekt_id, temp.ocena, temp.data }).ToList();
-            studenciBindingSource.DataSource = (from temp in doe.studenci select new { temp.student_id, temp.imie, temp.nazwisko, temp.nr_indexu, temp.kierunek, temp.grupa, temp.email, temp.haslo }).ToList();
-            
+            //ocenyprojektBindingSource.DataSource = (from temp in doe.oceny_projekt
+            //                     select new { temp.student_id, temp.projekt_id, temp.ocena, temp.data }).ToList();
+            //studenciBindingSource.DataSource = (from temp in doe.studenci select new { temp.student_id, temp.imie, temp.nazwisko, temp.nr_indexu, temp.kierunek, temp.grupa, temp.email, temp.haslo }).ToList();
+
         }
 
-        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        private void MainWindow_Load(object sender, EventArgs e)
         {
 
         }
 
         private void buttonZmienOcene_Click(object sender, EventArgs e)
         {
-            var query = from temp in doe.oceny_projekt where temp.student_id == 1 select temp;
-            foreach(var x in query)
-            {
-                if(textBoxOcena.Text != "")
-                    x.ocena = Convert.ToDecimal(textBoxOcena.Text);
-            }
-            try
-            {
-                doe.SaveChanges();
-            } catch (InvalidOperationException exc)
-            {
-                Console.WriteLine(exc);
-            }
+            //var query = from temp in doe.oceny_projekt where temp.student_id == 1 select temp;
+            //foreach(var x in query)
+            //{
+            //    if(textBoxOcena.Text != "")
+            //        x.ocena = Convert.ToDecimal(textBoxOcena.Text);
+            //}
+            //try
+            //{
+            //    doe.SaveChanges();
+            //} catch (InvalidOperationException exc)
+            //{
+            //    Console.WriteLine(exc);
+            //}
+        }
+
+        private void buttonStudenci_Click(object sender, EventArgs e)
+        {
+            Controls.Remove(tableLayoutPanel1);
+            dataGridViewStudents = new DataGridView();
+            dataGridViewStudents.Dock = DockStyle.Fill;
+            dataGridViewStudents.DataSource = (from temp in doe.oceny_przedmiot
+                                               select new { temp.student_id, temp.przedmiot_id, temp.ocena, temp.data }).ToList();
+            Controls.Add(dataGridViewStudents);
         }
     }
 }
